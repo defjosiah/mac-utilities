8da3c51706cf162cdd9f76ab82c5b919bcfea4e9
Josiah Grace <josiahbgrace@gmail.com>
2014-09-30 22:47:42 -0500

    added timer functionality

---
+++ timer/timer.py
@@ -0,0 +1,17 @@
+import time
+import sys
+import os
+
+def start_timer(minutes):
+    wd = os.getcwd()
+    for x in xrange(minutes):
+        time.sleep(60)
+        print x+1
+    os.system("osascript {0}".format(os.path.join(wd, "playpause.applescript")))
+    os.system("say timer finished")
+    os.system("osascript -e \'display notification \"Timer Finished\" with title \"Timer\"\'")
+    return
+
+if __name__ == '__main__':
+    start_timer(int(sys.argv[1]))
+
